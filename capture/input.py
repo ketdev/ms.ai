043cf6eedd12df54b06349c0f64e5c9f9914a0ec
ketdev <xhi2art@gmail.com>
2023-09-09 19:44:20 +0300

    adding virtual keys for arrows

--- capture/input.py
+++ capture/input.py
@@ -1,8 +1,15 @@
+import time
 import ctypes
 
 # Key event flags
 KEYEVENTF_KEYUP = 0x0002
 KEYEVENTF_SCANCODE = 0x0008
+
+# Virtual key codes
+VK_LEFT = 0x25
+VK_UP = 0x26
+VK_RIGHT = 0x27
+VK_DOWN = 0x28
 
 # C struct definitions
 PUL = ctypes.POINTER(ctypes.c_ulong)
@@ -36,20 +43,44 @@ class Input(ctypes.Structure):
     _fields_ = [("type", ctypes.c_ulong),
                 ("ii", Input_I)]
 
-def press_key(hex_key_code):
+def press_scan_key(hex_key_code):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
     ii_.ki = KeyBdInput(0, hex_key_code, KEYEVENTF_SCANCODE, 0, ctypes.pointer(extra))
     x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-def release_key(hex_key_code):
+def release_scan_key(hex_key_code):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
     ii_.ki = KeyBdInput(0, hex_key_code, KEYEVENTF_KEYUP | KEYEVENTF_SCANCODE, 0, ctypes.pointer(extra))
     x = Input(ctypes.c_ulong(1), ii_)
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-# Example: Press and release the 'A' key using its scan code
-press_key(0x1E)
-release_key(0x1E)
+def press_virtual_key(vk_code):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(vk_code, 0, 0, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+def release_virtual_key(vk_code):
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.ki = KeyBdInput(vk_code, 0, KEYEVENTF_KEYUP, 0, ctypes.pointer(extra))
+    x = Input(ctypes.c_ulong(1), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
+
+KEY_A = 0x1E # 'A'
+
+# # Example: Press and release the 'A' key using its scan code
+
+press_virtual_key(VK_RIGHT)
+press_scan_key(KEY_A)
+time.sleep(1.2)
+release_scan_key(KEY_A)
+release_virtual_key(VK_RIGHT)
+
+
