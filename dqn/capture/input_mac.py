9e1124c01b0a7d01c6f0a17e65928629d0651d5a
Keter <david.keter@hellosimply.com>
2023-10-08 15:35:42 +0300

    removing old version code

--- dqn/capture/input_mac.py
+++
@@ -1,7 +0,0 @@
-import Quartz
-
-def press_key(vkey):
-    Quartz.CGEventPost(Quartz.kCGHIDEventTap, Quartz.CGEventCreateKeyboardEvent(None, vkey, True))
-
-def release_key(vkey):
-    Quartz.CGEventPost(Quartz.kCGHIDEventTap, Quartz.CGEventCreateKeyboardEvent(None, vkey, False))
